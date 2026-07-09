eba8f5423603f60293dab786e0f2586e3ae23cef
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-07-09 12:22:08 +0400

    added trailing slash to /assets

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	fileServer1 := http.FileServer(http.Dir("."))
 	fileServer2 := http.FileServer(http.Dir("./assets"))
 	mux.Handle("/", fileServer1)
-	mux.Handle("/assets", http.StripPrefix("/assets/", fileServer2))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fileServer2))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
