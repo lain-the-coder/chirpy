2d15257b73a483180d74ed63de0e3c862824dd67
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-07-09 11:45:20 +0400

    Server running on 8080 port and serving a static file

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/", fileServer)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
