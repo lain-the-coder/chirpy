6a88a62fdd35182056e35646e621fd386c23ca5e
Ehab Ahmed (lain-the-coder) <ahmed.ehabj@gmail.com>
2026-07-10 11:22:48 +0400

    Added new endpoint POST /api/validate_chirp with json response and error handling

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -42,6 +44,59 @@ func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func HandlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type validateChirpRequest struct {
+		Body string `json:"body"`
+	}
+	type validateChirpResponse struct {
+		Valid bool `json:"valid"`
+	}
+	type validateChirpError struct {
+		Error string `json:"error"`
+	}
+	reqBody := validateChirpRequest{}
+	respBody := validateChirpResponse{}
+	errorBody := validateChirpError{}
+	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		errorBody.Error = "Something went wrong"
+		dat, err := json.Marshal(errorBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Write(dat)
+		return
+	}
+	if len(reqBody.Body) > 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		errorBody.Error = "Chirp is too long"
+		dat, err := json.Marshal(errorBody)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Write(dat)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	respBody.Valid = true
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Write(dat)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	cfg := &apiConfig{}
@@ -52,6 +107,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", HandlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", HandlerValidateChirp)
 	mux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/app/", http.StatusSeeOther)
 	})
